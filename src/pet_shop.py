23d16ecfb6c0bf83e12c7810e76d202d1bb9d78c
Douglas Russell <douglas.russell7@btinternet.com>
2021-10-21 16:23:41 +0100

    adds add_pet_to_customer

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -44,3 +44,6 @@ def remove_customer_cash(customer, amount):
 
 def get_customer_pet_count(customer):
     return len(customer["pets"])
+
+def add_pet_to_customer(customer, pet):
+    customer["pets"].append(pet)
